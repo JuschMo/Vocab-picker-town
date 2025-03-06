d518a5c90990b6dd3a840b0135ebab4b4f0f0f71
JuschMo <131524320+JuschMo@users.noreply.github.com>
2025-03-06 19:11:32 +0000

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
     const vocabulary = [
-        { german: "ein Geschäft.", english: "a shop." },
+        { german: "Ich wohne ...", english: "I live ..." },
+        { german: "in einem Dorf.", english: "in a village." },
+        { german: "in einer Kleinstadt.", english: "in a small town." },
+        { german: "in einer Großstadt.", english: "in a city." },
+        { german: "in der Stadtmitte.", english: "in the city centre." },
+        { german: "in den Bergen.", english: "in the mountains." },
+        { german: "am Stadtrand.", english: "on the outskirts." },
+        { german: "auf dem Land.", english: "in the countryside." },
+        { german: "an einem See.", english: "by a lake." },
+        { german: "an der Küste.", english: "on the coast." },
+        { german: "Das liegt ...", english: "It lies / is situated ..." },
+        { german: "im Norden von ...", english: "in the north of ..." },
+        { german: "im Osten von ...", english: "in the east of ..." },
+        { german: "im Süden von ...", english: "in the south of ..." },
+        { german: "im Westen von ...", english: "in the west of ..." },
+        { german: "Der Ort ist ...", english: "The place is ..." },
+        { german: "Die Gegend ist ...", english: "The region, area is ..." },
+        { german: "Die Landschaft ist ...", english: "The countryside is ..." },
+        { german: "besonders", english: "especially" },
+        { german: "echt", english: "really" },
+        { german: "ganz", english: "quite" },
+        { german: "Es gibt ...", english: "There is ..." },
+        { german: "einen Flughafen.", english: "an airport." },
+        { german: "eine Bank.", english: "a bank." },
+        { german: "eine Bibliothek.", english: "a library." },
+        { german: "eine Post.", english: "a post office." },
         { german: "ein Kino.", english: "a cinema." },
-        { german: "ein Krankenhaus.", english: "a hospital." }
+        { german: "ein Krankenhaus.", english: "a hospital." },
+        { german: "ein Museum.", english: "a museum." },
+        { german: "ein Schwimmbad.", english: "a swimming pool." },
+        { german: "viel Verkehr.", english: "a lot of traffic." }
     ];
 
     let currentWord = null;
